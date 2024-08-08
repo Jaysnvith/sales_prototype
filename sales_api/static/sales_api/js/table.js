663adc4aa5893ba791637c1077ab4057fafb0149
Jaysnvith <panjim0b@gmail.com>
2024-08-08 15:42:42 +0700

    Change responsive to scrollX

--- sales_api/static/sales_api/js/table.js
+++ sales_api/static/sales_api/js/table.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#mastertable').DataTable({
         keys: true,
         stateSave: true,
+        scrollX: true,
         columnDefs: [
             { orderable: false, targets: 'disable-sorting' }
         ],
