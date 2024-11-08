57ca4d92e318c37d279290063c2749b65aa4a6cc
Jaysnvith <panjim0b@gmail.com>
2024-11-08 16:33:39 +0700

    Added API keys

--- sales_prototype/settings.py
+++ sales_prototype/settings.py
@@ -25,8 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+EXCHANGE_API_KEY = os.getenv("EXCHANGE_API_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'False'
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
